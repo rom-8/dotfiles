32c950920d03fe8def0dab024feda8541de1dd4c
rom <romberg.mit@gmail.com>
2015-03-21 13:37:31 +0900

    tmux powerline追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vimrc_lightline ~/.vimrc_lightline
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tmux-powerlinerc ~/.tmux-powerlinerc
